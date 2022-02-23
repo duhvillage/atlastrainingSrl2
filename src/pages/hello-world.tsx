7c9d32b9eea87c1078444c7c1361a5fda7500d7f
Duhvillage <clydemelendez@gmail.com>
2022-02-23 13:14:21 -0600

    Hello world test

---
+++ src/pages/hello-world.tsx
@@ -0,0 +1,14 @@
+import { getNextStaticProps } from '@faustjs/next';
+import { client } from 'client';
+import { Footer, Header, Hero } from 'components';
+
+
+export default function Page() {
+  
+
+  return (
+    <>
+     <h1>Hello World</h1>
+    </>
+  );
+}
